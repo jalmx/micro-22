85a75db521d64dc4ae8ae520f9c31cc36658c0d9
jalmx <jalmx@users.noreply.github.com>
2022-04-11 23:22:33 +0000

    deploy: 01f25bd1a1d037b7020ead0d67e23f4a63156850

--- capitulo_2/gui/law_ohm/gui.py
+++ capitulo_2/gui/law_ohm/gui.py
@@ -1,8 +1,42 @@
 from tkinter import *
 
-root = Tk()
-t = Text(root)
+def build_gui():
+    root = Tk()
+    Label(root, text="Ohm Law Calculator", font=("Hack", 20)).pack()
+    
+    container_options = Frame(root)
+    container_options.pack()
+    option_volt = Radiobutton(container_options, text="Voltaje")
+    option_current = Radiobutton(container_options, text="Corriente")
+    option_resistence = Radiobutton(container_options, text="Resistencia")
+    
+    container_input_one = Frame(root)
+    label_one_line = Label(container_input_one)
+    label_one_line.pack()
+    input_one_line = Entry(container_input_one)
+    input_one_line.pack()
+    unit_one_line = Label(container_input_one)
+    unit_one_line.pack()
+    
+    container_input_two = Frame(root)
+    label_two_line = Label(container_input_two)
+    label_one_line.pack()
+    input_two_line = Entry(container_input_two)
+    input_one_line.pack()
+    unit_two_line = Label(container_input_two)
+    unit_one_line.pack()
+    
+    btn_calculate = Button(root, text="Calculate")
+    btn_calculate.pack()
+    
+    label_result = Label(root, text="1.0A", font=("Hack", 20) )
+    label_result.pack()
+    
+    return root
+    
 
-print(dict(t))
+def init_app():
+    build_gui().mainloop()
 
-root.mainloop()
+if __name__ == "__main__":
+    init_app()
