4be87f0b94504c9f02085914bbef48d5b1329940
jalmx <jalmx@users.noreply.github.com>
2022-05-30 21:12:29 +0000

    deploy: 8c1fc37bf7eb681eace68aef4beda6c5e218443d

--- capitulo_2/Ejemlos GUI/law_ohm/gui.py
+++ capitulo_2/Ejemlos GUI/law_ohm/gui.py
@@ -1,13 +1,13 @@
 from tkinter import *
 from tkinter import messagebox
 
-import law_ohm
+import ohms_law
 
 VOLT_OPTION = 0
 CURRENT_OPTION = 1
 RESISTANCE_OPTION = 2
 
-menu_list = ["Voltaje", "Corriente", "Resistencia"]
+menu_list = ["Voltage", "Current", "Resistance"]
 unit_list = ["V", "A", "Ω"]
 
 def change_ui():
@@ -27,32 +27,34 @@ def change_ui():
         label_unit_one.set(unit_list[VOLT_OPTION])
         label_str_two.set(menu_list[CURRENT_OPTION])
         label_unit_two.set(unit_list[CURRENT_OPTION])
-    
+
 
 def calculate():
     option = option_selected.get()
     value = 0.0
-    
+
     value_one = value_entry_one.get()
     value_two = value_entry_two.get()
-    
+
     if not len(value_one) or not len(value_two):
         messagebox.showerror("Error", "No puede haber campos vacíos")
         return
-    # if  not value_one.isnumeric() or not value_two.isnumeric():
-    #     messagebox.showerror("Error", "No puede ser una letra")
-    #     return
-    
+
     value_one = float(value_one)
     value_two = float(value_two)
-    
+
+    if value_two == 0 and (option == CURRENT_OPTION or option == RESISTANCE_OPTION):
+        messagebox.showerror("Error", "No puede ser cero el segundo valor, porque es infinito")
+        value_label_result.set("-")
+        return
+
     if option == VOLT_OPTION:
-        value = law_ohm.volt(value_one,value_two)
+        value = ohms_law.voltage(value_one,value_two)
     elif option == CURRENT_OPTION:
-        value = law_ohm.current(value_one,value_two)
+        value = ohms_law.current(value_one,value_two)
     elif option == RESISTANCE_OPTION:
-        value = law_ohm.resistance(value_one,value_two)
-        
+        value = ohms_law.resistance(value_one,value_two)
+
     value = "{0:1.4f} {1}".format(value, unit_list[option])
     value_label_result.set(value)
 
@@ -61,7 +63,7 @@ def build_gui():
 
     Returns:
         Tk: Main windows from app
-    """    
+    """
     root = Tk()
     root.title("Ohm Law Calculator")
     root.resizable(0, 0)
@@ -70,7 +72,7 @@ def build_gui():
         expand=True, fill=BOTH
     )
 
-    container_options = LabelFrame(root, text="Calcular", bd=1, relief=SUNKEN)
+    container_options = LabelFrame(root, text="Calculate", bd=1, relief=SUNKEN)
     container_options.pack(expand=True, fill=BOTH, padx=8, pady=8, ipadx=8, ipady=8)
 
     global option_selected
@@ -97,11 +99,11 @@ def build_gui():
     global label_unit_one
     label_unit_one= StringVar()
     label_unit_one.set(unit_list[CURRENT_OPTION])
-    
+
     global value_entry_one
     value_entry_one = StringVar()
-    value_entry_one.set("0.0") 
-   
+    value_entry_one.set("0.0")
+
     container_input_one = Frame(root)
     container_input_one.pack(expand=True, fill=BOTH, padx=8, pady=8, ipadx=8, ipady=8)
     label_one_line = Label(container_input_one, textvariable=label_str_one)
@@ -119,7 +121,7 @@ def build_gui():
     label_unit_two.set(unit_list[RESISTANCE_OPTION])
     global value_entry_two
     value_entry_two = StringVar()
-    value_entry_two.set("0.0") 
+    value_entry_two.set("0.0")
     container_input_two = Frame(root)
     container_input_two.pack(expand=True, fill=BOTH, padx=8, pady=8, ipadx=8, ipady=8)
     label_two_line = Label(container_input_two, textvariable=label_str_two)
@@ -143,11 +145,11 @@ def build_gui():
 
 def init_app():
     """Function to exec the app
-    """    
+    """
     build_gui().mainloop()
 
 
 if __name__ == "__main__":
     """function main to exec the app in test
-    """    
+    """
     init_app()
